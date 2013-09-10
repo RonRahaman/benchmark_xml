4ba0cba36d1e9592499682b0ec77d63606c5992e
Ron Rahaman <rahaman@cs.uchicago.edu>
2013-09-10 11:47:40 -0500

    moved exising builds into v0_v1_fox directory

--- batch.pl
+++ batch.pl
@@ -14,7 +14,7 @@ for (my $i = 4; $i <= 1200000 ; $i = 2*$i) {
   # Number of trials for this xml file
   for (my $j = 1; $j <= 3; $j++) {
     foreach (@modes) {
-      system("./main ./ $_"); 
+      system("v0_v1_fox/main ./ $_"); 
       unless (WIFEXITED(${^CHILD_ERROR_NATIVE})) {
         die "\nexecution of ./batch.pl has been terminated ".
           "(because ./main exited with status ${^CHILD_ERROR_NATIVE})\n";
