b91f9d10cd494fb567e580d59135b6e961d4b2b2
Ron Rahaman <rahaman@cs.uchicago.edu>
2013-09-10 15:27:00 -0500

    added separate support for v0

--- batch.pl
+++ batch.pl
@@ -8,12 +8,14 @@ my @command = (
   'v0_v1_fox/main ./ fox_dom', 
   'v0_v1_fox/main ./ xml_fortran_old', 
   'v0_v1_fox/main ./ xml_fortran_new',
-  'v3/main');
+  'v3/main',
+  'v0/main',
+);
 
 unlink("benchmark_xml.txt");
 
 # Range for the number of lines in each xml file
-for (my $i = 4; $i <= 1200000 ; $i = 2*$i) {
+for (my $i = 2; $i <= 1200000 ; $i = 2*$i) {
   system("./generate_geometry.pl $i");
   # Number of trials for this xml file
   for (my $j = 1; $j <= 1; $j++) {
